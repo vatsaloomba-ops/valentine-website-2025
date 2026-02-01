1a1d633d984b933576f3520fc062134fb6ffae3b
vatsaloomba-ops <vatsaloomba@gmail.com>
2026-02-01 12:49:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/diny2gjwx/video/upload/v1769930304/ost_call_me_by_your_name_-_mystery_of_love__mp3.pm_u4ftyu.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
